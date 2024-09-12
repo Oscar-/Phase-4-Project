6724224751868e5398c06e520df7f98e177f742a
Daphney <daphneyalexandre@hotmail.com>
2024-09-12 01:02:41 -0400

    updated readme

--- server/migrations/versions/85c949b38270_.py
+++ server/migrations/versions/85c949b38270_.py
@@ -1,8 +1,8 @@
-"""yur
+"""empty message
 
-Revision ID: 23d6876174a2
+Revision ID: 85c949b38270
 Revises: 
-Create Date: 2024-09-11 20:32:20.414868
+Create Date: 2024-09-11 23:41:23.869964
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '23d6876174a2'
+revision = '85c949b38270'
 down_revision = None
 branch_labels = None
 depends_on = None
